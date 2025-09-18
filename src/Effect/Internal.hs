2b22874f87c8588419759f797ceaf1d0310da515
Choi-Suhyeon <1AzyF0x@protonmail.com>
2025-09-18 14:28:13 +0900

    feat: introduce Effect.Persistence with readData/writeData

--- src/Effect/Internal.hs
+++ src/Effect/Internal.hs
@@ -1,1 +1,12 @@
-module Effect.Internal () where
+module Effect.Internal (tee, teeM, (>!)) where
+
+infixl 1 >!
+
+tee :: Functor f => (a -> f b) -> a -> f a
+tee = liftA2 (<$) id
+
+teeM :: Monad m => (a -> m b) -> m a -> m a
+teeM = (=<<) . tee
+
+(>!) :: Monad m => m a -> (a -> m b) -> m a
+(>!) = flip teeM
--- src/Effect/Persistence.hs
+++ src/Effect/Persistence.hs
@@ -1,1 +1,46 @@
-module Effect.Persistence () where
+module Effect.Persistence (readData, writeData) where
+
+import Prelude hiding (readFile)
+
+import System.Directory.Internal (ignoreIOExceptions)
+import Control.Exception         (bracketOnError)
+import System.Directory  
+    ( XdgDirectory(..)         , getXdgDirectory , doesFileExist
+    , createDirectoryIfMissing , removeFile      , renameFile
+    )
+import Data.ByteString           (ByteString, readFile, hPutStr)
+import System.FilePath           ((</>), (<.>))
+import Control.Monad             (when)
+import System.IO                 (openBinaryTempFile, hClose)
+
+import Effect.Internal
+
+readData :: IO ByteString
+readData = (</> dataFileName) <$> getDataDirectory >>= readFile
+
+writeData :: ByteString -> IO ()
+writeData bs = getDataDirectory >>= \dir ->
+    bracketOnError
+        (openBinaryTempFile dir $ dataFileName <.> "tmp")
+        (\(tmp, h) -> hClose h *> ignoreIOExceptions (removeFile tmp))
+        \(tmp, h) -> do
+            hPutStr h bs
+            hClose h
+
+            let
+                fullPath = dir </> dataFileName
+
+            exists <- doesFileExist fullPath
+
+            when exists $ removeFile fullPath
+            renameFile tmp fullPath
+
+getDataDirectory :: IO FilePath
+getDataDirectory = getXdgDirectory XdgData dataDirectoryName >! createDirectoryIfMissing True
+
+dataDirectoryName :: String
+dataDirectoryName = "todo"
+
+dataFileName :: String
+dataFileName = "todo.dat"
+
